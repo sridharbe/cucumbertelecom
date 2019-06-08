3a5e3e139655fd2f0698b7e2dfcae7cf86e39143
sridharbe <srriidhar@gmail.com>
2019-06-08 17:38:19 +0530

    maven compiler2

--- src/test/java/cucumber1/TestRunner.java
+++ src/test/java/cucumber1/TestRunner.java
@@ -7,7 +7,7 @@ import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features="src\\test\\resources\\Features",tags={},dryRun=false,glue="cucumber1",
-plugin={"rerun:src\\main\\resources\\failed.txt","jason:target\\jason_report\\cucumber.jason"},monochrome=true)
+plugin={"rerun:src\\main\\resources\\failed.txt","jason:target"},monochrome=true)
 public class TestRunner {
 
 	
