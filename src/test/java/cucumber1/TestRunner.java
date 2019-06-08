969d4f4aa3e9f4ce04bcf175ea0e8b4ef9f0f492
sridharbe <srriidhar@gmail.com>
2019-06-08 13:08:00 +0530

    add jason path

--- src/test/java/cucumber1/TestRunner.java
+++ src/test/java/cucumber1/TestRunner.java
@@ -7,7 +7,8 @@ import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features="src\\test\\resources\\Features",tags={},dryRun=false,glue="cucumber1",
-plugin={"rerun:src\\main\\resources\\failed.txt","html:target"},monochrome=true)
+plugin={"rerun:src\\main\\resources\\failed.txt","jason:target\\jason_report\\cucumber.jason"},monochrome=true)
 public class TestRunner {
+
 	
 	}
